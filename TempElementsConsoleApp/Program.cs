5dfe85a5a770ba08869c201d2b6c1761289bb403
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-05-18 21:41:00 +0200

    Zadanie 1

--- TempElementsConsoleApp/Program.cs
+++ TempElementsConsoleApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TempElementsLib;
+
+class Program
+{
+    static void Main()
+    {
+        using (var tempFile = new TempFile())
+        {
+            tempFile.AddText("Hello, temporary file!"); 
+            Console.WriteLine($"File is created: {tempFile.FileInfo.FullName}");
+        }
+        Console.WriteLine("File was deleted after using.");
+
+        var tempFile2 = new TempFile();
+        tempFile2.AddText("Test Dispose."); 
+        tempFile2.Dispose();
+        try
+        {
+            tempFile2.AddText("Should throw an exception."); 
+        }
+        catch (ObjectDisposedException ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+
+        var tempFile3 = new TempFile();
+        try
+        {
+            tempFile3.AddText("Test without using.");
+        }
+        finally
+        {
+            tempFile3.Dispose();
+        }
+
+        Console.ReadLine();
+    }
+}
--- TempElementsLib/TempFile.cs
+++ TempElementsLib/TempFile.cs
@@ -1,10 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
-namespace TempElementsLib
+namespace TempElementsLib 
 {
-    class TempFile
+    public class TempFile : IDisposable, ITempFile
     {
+        private readonly FileStream fileStream;
+        private readonly FileInfo fileInfo;
+
+        private bool disposed = false;
+
+        public TempFile()
+        {
+            string tempFilePath = Path.GetTempFileName();
+            fileInfo = new FileInfo(tempFilePath);
+            fileStream = new FileStream(tempFilePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        }
+
+        public TempFile(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath) || !Path.IsPathRooted(filePath))
+                throw new ArgumentException("Invalid file path.", nameof(filePath));
+
+            fileInfo = new FileInfo(filePath);
+            if (fileInfo.Exists) fileInfo.Delete();
+            fileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+        }
+
+        public FileStream FileStream
+        {
+            get { return fileStream; }
+        }
+
+        public FileInfo FileInfo
+        {
+            get { return fileInfo; }
+        }
+
+        public void AddText(string value)
+        {
+            if (disposed) throw new ObjectDisposedException(nameof(TempFile));
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fileStream.Write(info, 0, info.Length);
+            fileStream.Flush();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    fileStream.Close();
+                }
+                fileInfo.Delete();
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~TempFile()
+        {
+            Dispose(false);
+        }
     }
+
+    public interface ITempFile : ITempElement
+    {
+        FileStream FileStream { get; }
+        FileInfo FileInfo { get; }
+    }
+
+    public interface ITempElement { }
 }
