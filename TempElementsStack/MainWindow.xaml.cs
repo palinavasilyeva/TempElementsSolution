92285681956379a507d4193d57ec8543790b85f6
palina.vasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-05-22 13:13:56 +0200

    5

--- TempElementsStack/MainWindow.xaml.cs
+++ TempElementsStack/MainWindow.xaml.cs
@@ -1,23 +1,64 @@
-﻿using System.Text;
+﻿//using System.Text;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+//namespace TempElementsStack;
+
+///// <summary>
+///// Interaction logic for MainWindow.xaml
+///// </summary>
+//public partial class MainWindow : Window
+//{
+//    public MainWindow()
+//    {
+//        InitializeComponent();
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TempElementsLib;
 
-namespace TempElementsStack;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace WpfTextEditor
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private Stack<TempTxtFile> history = new Stack<TempTxtFile>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveState_Click(object sender, RoutedEventArgs e)
+        {
+            // Zachowaj aktualny stan do pliku tymczasowego
+            var tempFile = new TempTxtFile();
+            tempFile.Write(TextEditor.Text);
+            history.Push(tempFile);
+            MessageBox.Show("Stan zapisany.", "Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Undo_Click(object sender, RoutedEventArgs e)
+        {
+            if (history.Count == 0)
+            {
+                MessageBox.Show("Brak zapisanych stanów do cofnięcia.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var lastState = history.Pop();
+            string content = lastState.ReadAllText();
+            lastState.Dispose();
+            TextEditor.Text = content;
+        }
     }
 }
